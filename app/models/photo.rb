0aea7246730350c1c3b76fe5fa4df0653c232667
Ricardo Juarez <146899774+jrzricardo@users.noreply.github.com>
2024-06-22 17:19:18 +0000

    refactored Photo indirect associations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -19,56 +19,64 @@ class Photo < ApplicationRecord
   
   ## Direct associations
 
-  # Photo#poster: returns a row from the users table associated to this photo by the owner_id column
+        # Photo#poster: returns a row from the users table associated to this photo by the owner_id column
 
-  # Photo#comments: returns rows from the comments table associated to this photo by the photo_id column
+        # Photo#comments: returns rows from the comments table associated to this photo by the photo_id column
 
-  # Photo#likes: returns rows from the likes table associated to this photo by the photo_id column
+        # Photo#likes: returns rows from the likes table associated to this photo by the photo_id column
 
   ## Indirect associations
 
-  # Photo#fans: returns rows from the users table associated to this photo through its likes
+        # Photo#fans: returns rows from the users table associated to this photo through its likes
 
-  def poster
-    my_owner_id = self.owner_id
+        # def poster
+        #   my_owner_id = self.owner_id
 
-    matching_users = User.where({ :id => my_owner_id })
+        #   matching_users = User.where({ :id => my_owner_id })
 
-    the_user = matching_users.at(0)
+        #   the_user = matching_users.at(0)
 
-    return the_user
-  end
-
-  def comments
-    my_id = self.id
-
-    matching_comments = Comment.where({ :photo_id => self.id })
-
-    return matching_comments
-  end
-
-  def likes
-    my_id = self.id
-
-    matching_likes = Like.where({ :photo_id => self.id })
-
-    return matching_likes
-  end
-
-  def fans
-    my_likes = self.likes
+        #   return the_user
+        # end
     
-    array_of_user_ids = Array.new
+  belongs_to(:poster, class_name: "User", foreign_key: "owner_id")
 
-    my_likes.each do |a_like|
-      array_of_user_ids.push(a_like.fan_id)
-    end
+        # def comments
+        #   my_id = self.id
 
-    matching_users = User.where({ :id => array_of_user_ids })
+        #   matching_comments = Comment.where({ :photo_id => self.id })
 
-    return matching_users
-  end
+        #   return matching_comments
+        # end
 
+  has_many(:comments)
+
+        # def likes
+        #   my_id = self.id
+
+        #   matching_likes = Like.where({ :photo_id => self.id })
+
+        #   return matching_likes
+        # end
+
+  has_many(:likes)
+  
+          # def fans
+          #   my_likes = self.likes
+            
+          #   array_of_user_ids = Array.new
+
+          #   my_likes.each do |a_like|
+          #     array_of_user_ids.push(a_like.fan_id)
+          #   end
+
+          #   matching_users = User.where({ :id => array_of_user_ids })
+
+          #   return matching_users
+          # end
+  
+  has_many(:fans, through: :likes)
+  
   def fan_list
     my_fans = self.fans
 
